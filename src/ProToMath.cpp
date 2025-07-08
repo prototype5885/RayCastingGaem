0054d326b08a36ca846dc006c0e60fb5aceb4c4c
ProToType <prototype585@protonmail.com>
2025-07-08 12:40:59 +0200

    removed pointless math

--- src/ProToMath.cpp
+++ src/ProToMath.cpp
@@ -1,6 +1,5 @@
 #include <cmath>
 #include <cstdlib>
-// #define ONE_RAD (3.14159 / 180.0);
 
 float lerpf(float from, float to, float percentage) { return from + (to - from) * percentage; }
 
@@ -8,16 +7,7 @@ float deg2rad(float num) { return num * (M_PI / 180.0); }
 
 float rad2deg(float num) { return num * 57.29578; }
 
-float sqr(float num) { return num * num; }
-
 int clampi(int d, int min, int max) {
   const int t = d < min ? min : d;
   return t > max ? max : t;
 }
-
-// int RandomInRange(int min, int max)
-// {
-//   return (random() % (max - min + 1)) + min;
-// }
-
-int RandomMax(int max) { return rand() % max; }
--- src/ProToMath.h
+++ src/ProToMath.h
@@ -4,9 +4,6 @@
 float lerpf(float from, float to, float percentage);
 float deg2rad(float num);
 float rad2deg(float num);
-float sqr(float num);
 int clampi(int d, int min, int max);
-// int RandomInRange(int min, int max);
-int RandomMax(int max);
 
 #endif

a8804f893a452fc7d1d552efce5802b4173cb3a3
ProToType <prototype585@protonmail.com>
2025-07-09 19:00:21 +0200

    now static links properly and copies sdl2.dll over

--- build_windows.bat
+++ build_windows.bat
@@ -6,10 +6,10 @@ set SDL2_VERSION=2.32.8
 set OUTPUT=game.exe
 set BUILD_DIR=build_windows
 
-echo Compiling...
-
+echo Copying game assets...
 if not exist "%BUILD_DIR%\textures" xcopy textures "%BUILD_DIR%\textures" /E /I /H /K /Y
 
+echo Compiling...
 echo Building Docker image...
 docker build -f dockerfile_windows -t sdl-builder --build-arg SDL2_VERSION=%SDL2_VERSION% --build-arg TARGET=%TARGET% .
 
@@ -25,7 +25,7 @@ for %%f in (src\*.cpp) do (
 )
 
 echo Running compilation...
-docker run --rm -v "%cd%:/app" -w /app sdl-builder bash -c "%TARGET%-g++ -Wall -Wextra -O3 %FILES% -o %BUILD_DIR%/%OUTPUT% -lmingw32 -lSDL2main -lSDL2 -static-libstdc++"
+docker run --rm -v "%cd%:/app" -w /app sdl-builder bash -c "%TARGET%-g++ -Wall -Wextra -O3 %FILES% -o %BUILD_DIR%/%OUTPUT% -lmingw32 -lSDL2main -lSDL2 -static-libgcc -static-libstdc++ && cp /usr/%TARGET%/bin/SDL2.dll %BUILD_DIR%"
 
 if %ERRORLEVEL% EQU 0 (
     echo Build successful into folder: %BUILD_DIR%

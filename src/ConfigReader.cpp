527ccd90112ef7049329f8b6dbb673a8c7c12508
ProToType <prototype585@protonmail.com>
2025-07-12 20:37:22 +0200

    made config file reading safer

--- src/ConfigReader.cpp
+++ src/ConfigReader.cpp
@@ -1,5 +1,6 @@
 #include "ConfigReader.h"
 #include <fstream>
+#include <iostream>
 #include <sstream>
 #include <string>
 
@@ -14,47 +15,56 @@ Config ReadConfigFile() {
   cfg.linearFiltering = false;
 
 #ifndef __EMSCRIPTEN__
-
-  const char *filename = "config.txt";
-
-  ifstream fileExists(filename);
-  if (!fileExists.good()) {
-    ofstream file(filename);
-    file << "fullscreen=false\n";
-    file << "width=1920\n";
-    file << "height=1080\n";
-    file << "resolutionPercentage=100\n";
-    file << "linearFiltering=false\n";
-  }
-
-  string line;
-  ifstream file(filename);
-
-  while (getline(file, line)) {
-    if (line.empty() || line[0] == '#')
-      continue;
-
-    istringstream iss(line);
-    string key, value;
-
-    if (getline(iss, key, '=') && getline(iss, value)) {
-      try {
-        if (key == "fullscreen") {
-          cfg.fullscreen = (value == "true");
-        } else if (key == "width") {
-          cfg.width = stoi(value);
-        } else if (key == "height") {
-          cfg.height = stoi(value);
-        } else if (key == "resolutionPercentage") {
-          cfg.resolutionPercentage = stoi(value);
-        } else if (key == "linearFiltering") {
-          cfg.linearFiltering = (value == "true");
+  try {
+    const char *filename = "config.txt";
+    {
+      ifstream fileExists(filename);
+      if (!fileExists.good()) {
+        ofstream fileWrite(filename);
+        if (!fileWrite.good()) {
+          throw("Couldn't create config file");
         }
-      } catch (const exception &e) {
+
+        fileWrite << "fullscreen=false\n";
+        fileWrite << "width=1920\n";
+        fileWrite << "height=1080\n";
+        fileWrite << "resolutionPercentage=100\n";
+        fileWrite << "linearFiltering=false\n";
       }
     }
-  }
+    {
+      ifstream fileRead(filename);
+      if (!fileRead.good()) {
+        throw("Can't read config file");
+      }
 
+      string line;
+      while (getline(fileRead, line)) {
+        if (line.empty() || line[0] == '#')
+          continue;
+
+        istringstream iss(line);
+        string key, value;
+
+        if (getline(iss, key, '=') && getline(iss, value)) {
+          if (key == "fullscreen") {
+            cfg.fullscreen = (value == "true");
+          } else if (key == "width") {
+            cfg.width = stoi(value);
+          } else if (key == "height") {
+            cfg.height = stoi(value);
+          } else if (key == "resolutionPercentage") {
+            cfg.resolutionPercentage = stoi(value);
+          } else if (key == "linearFiltering") {
+            cfg.linearFiltering = (value == "true");
+          }
+        }
+      }
+    }
+  } catch (const char *ex) {
+    std::cout << ex << endl;
+    exit(1);
+  }
 #endif
 
   return cfg;

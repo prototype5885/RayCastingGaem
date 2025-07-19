ea65469dd09514d0a8ce0f249d4d89433cad79b9
ProToType <prototype585@protonmail.com>
2025-07-19 21:29:21 +0200

    optimized geometry functions, removed the need for geometry.cpp, added more calculations like dot product

--- src/geometry.h
+++ src/geometry.h
@@ -1,9 +1,9 @@
 #ifndef GEOMETRY_H
 #define GEOMETRY_H
+#include <cmath>
 #include <cstdint>
 
 namespace geometry {
-
 struct Vector2i {
   int x, y;
 
@@ -28,6 +28,8 @@ struct Vector2 {
 
   Vector2 operator+(const Vector2 &other) const { return {x + other.x, y + other.y}; }
   Vector2 operator-(const Vector2 &other) const { return {x - other.x, y - other.y}; }
+  constexpr float Length() const { return x * x + y * y; }
+  constexpr float LengthSquared() const { return std::sqrt(Length()); }
 };
 
 typedef struct {
@@ -41,12 +43,38 @@ typedef struct {
   float d;
 } Line2D;
 
-float lerpf(float from, float to, float percentage);
-float deg2rad(float num);
-float rad2deg(float num);
-// Vector2 LinesIntersectionPoint(Vector2 a, Vector2 b, Vector2 c, Vector2 d);
-float EuclideanDistance(Vector2 from, Vector2 to);
-void PrintVector2(Vector2 vec);
+constexpr float lerpf(const float from, const float to, const float percentage) { return from + (to - from) * percentage; };
+
+constexpr float deg2rad(const float num) { return num * (static_cast<float>(M_PI) / 180.0f); };
+
+constexpr float rad2deg(const float num) { return num * 57.29578f; };
+
+inline Vector2 Normalize(const Vector2 &v) {
+  const float length = v.LengthSquared();
+  if (length == 0.0f) {
+    return {0.0f, 0.0f}; // so won't divide by zero
+  }
+  return {v.x / length, v.y / length};
+}
+
+inline float EuclideanDistance(const Vector2 &from, const Vector2 &to) {
+  const Vector2 dir = to - from;
+  return dir.LengthSquared();
+};
+
+constexpr float DotProduct(const Vector2 &a, const Vector2 &b) { return a.x * b.x + a.y * b.y; }
+
+inline Vector2 GetForwardVector(const float rotRad) { return {cosf(rotRad), sinf(rotRad)}; }
+
+inline bool IsFacingTarget(const Vector2 &fromPos, const Vector2 &forwardVector, const Vector2 &targetPos, const float limit) {
+  const float dotProduct = DotProduct(forwardVector, Normalize(targetPos - fromPos));
+
+  if (dotProduct > limit) {
+    return true;
+  }
+
+  return false;
+}
 } // namespace geometry
 
 #endif

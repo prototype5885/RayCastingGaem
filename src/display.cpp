170a0074eebbb50602c250207651fd00f3a24d85
ProToType <prototype585@protonmail.com>
2025-07-20 00:18:55 +0200

    speedup by moving add pixel functions into header as inline

--- src/display.cpp
+++ src/display.cpp
@@ -1,8 +1,6 @@
 #include "display.h"
-#include "colors.h"
 
 #include <cstdint>
-#include <cstdio>
 
 namespace display {
 float resScale = 1.0f;
@@ -11,19 +9,3 @@ int height = 1080;
 int size = width * height;
 uint32_t *pixels;
 } // namespace display
-
-void display::AddPixelToBuffer(const int x, const int y, const uint32_t color) {
-  const int i = y * width + x;
-  if (0 <= x && x < width && 0 <= y && y < height) {
-    if (color::GetAlpha(color) == UINT8_MAX) {
-      pixels[i] = color;
-    }
-  } else {
-    // printf("Tried to put pixel out of display bounds at index: %d\n", i);
-  }
-}
-
-void display::AddPixelToBufferUnsafe(const int x, const int y, const uint32_t color) {
-  // display::pixels[y * display::width + x] = vga_palette[color];
-  pixels[y * width + x] = color;
-}
--- src/display.h
+++ src/display.h
@@ -1,6 +1,8 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include "colors.h"
+
 #include <cstdint>
 
 namespace display {
@@ -8,8 +10,21 @@ extern float resScale;
 extern int width, height, size;
 extern uint32_t *pixels;
 
-void AddPixelToBuffer(int x, int y, uint32_t color);
-void AddPixelToBufferUnsafe(int x, int y, uint32_t color);
+inline void AddPixelToBuffer(const int x, const int y, const uint32_t color) {
+  const int i = y * width + x;
+  if (0 <= x && x < width && 0 <= y && y < height) {
+    if (color::GetAlpha(color) == UINT8_MAX) {
+      pixels[i] = color;
+    }
+  } else {
+    // printf("Tried to put pixel out of display bounds at index: %d\n", i);
+  }
+};
+
+inline void AddPixelToBufferUnsafe(const int x, const int y, const uint32_t color) {
+  // display::pixels[y * display::width + x] = vga_palette[color];
+  pixels[y * width + x] = color;
+};
 } // namespace display
 
 #endif
